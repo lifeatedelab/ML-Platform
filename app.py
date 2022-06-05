9182b5c40c404f678da514818a05bb5ccf847e30
rizalw <rizalwidyananda@gmail.com>
2022-06-05 07:13:04 +0700

    Success memisahkan routing berdasarkan fitur

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template, url_for, redirect
-from flask_login import login_user, LoginManager, login_required, logout_user, current_user
-from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_required
 from models import db, User
-from forms import LoginForm, RegisterForm
+from auth.auth import auth, bcrypt
 
 app = Flask(__name__)
+app.register_blueprint(auth, url_prefix="/auth")
 db.init_app(app)
-bcrypt = Bcrypt(app)
+bcrypt.init_app(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 #harus ada ini karena flask login pake session
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login' #jika user belom login, kelempar ke view ini
+login_manager.login_view = 'auth.login' #jika user belom login, kelempar ke view ini
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -23,41 +23,10 @@ def load_user(user_id):
 def home():
     return render_template('home.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('dashboard'))
-    return render_template('login.html', form=form)
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
-
-    return render_template('register.html', form=form)
-
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
     return render_template('dashboard.html')
-
-
-@app.route('/logout', methods=['GET', 'POST'])
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
